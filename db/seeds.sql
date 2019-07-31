e9a8bf1e4e8d91e50674a568ae3a9dd51583e161
AJW <ajweber102@gmail.com>
2019-07-31 09:59:47 -0400

    Created and ran schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Krabby Patty", FALSE); 
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("McBomb", FALSE); 
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Burger on Soggy Toast", FALSE); 
