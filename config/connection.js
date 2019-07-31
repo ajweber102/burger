30d474330d11a06d9a44d915119dfb2d1e3ed414
AJW <ajweber102@gmail.com>
2019-07-31 18:27:18 -0400

    Added JawsDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,18 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "P3pp3r102!",
-  database: "burgers_db"
-});
+var connection;
 
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Connected!');
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection ({
+    host: "localhost",
+    user: "root",
+    password: "P3pp3r102!",
+    database: "burgers_db"
+  });
+};
 
+connection.connect();
 module.exports = connection;
